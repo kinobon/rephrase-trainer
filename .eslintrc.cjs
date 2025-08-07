a75a8660229f9191a0885bf2b42f5b9699cf4371
kinobon2021 <kinobon2021@gmail.com>
2025-08-07 21:21:31 +0900

    eslintエラー回避のため追加

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2021: true },
+  env: { browser: true, es2021: true, node: true },
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint", "solid", "prettier"],
   extends: [
