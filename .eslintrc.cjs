ee46284d6c4e6f1c9db1161a74e7ad9c8a5982a2
kinobon2021 <kinobon2021@gmail.com>
2025-08-07 21:16:15 +0900

    chore: setup ESLint, Prettier, husky, and TailwindCSS

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2021: true },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "solid", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:solid/recommended",
+    "plugin:prettier/recommended",
+  ],
+  ignorePatterns: ["dist", "node_modules", "target", "*.css", "*.json"],
+  rules: {
+    "linebreak-style": ["error", "windows"],
+  },
+};
---
+++ .prettierrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  singleQuote: false,
+  semi: true,
+  trailingComma: "es5",
+  printWidth: 100,
+  plugins: ["prettier-plugin-tailwindcss"],
+  endOfLine: "crlf",
+};
+exports = {
+  singleQuote: false,
+  semi: true,
+  trailingComma: "es5",
+  printWidth: 100,
+  plugins: ["prettier-plugin-tailwindcss"],
+};
