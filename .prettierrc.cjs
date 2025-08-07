a9f092529343c0edd2a1420c137300b37a5e6684
kinobon2021 <kinobon2021@gmail.com>
2025-08-07 21:24:36 +0900

    不要な設定削除

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -4,12 +4,4 @@ module.exports = {
   trailingComma: "es5",
   printWidth: 100,
   plugins: ["prettier-plugin-tailwindcss"],
-  endOfLine: "crlf",
-};
-exports = {
-  singleQuote: false,
-  semi: true,
-  trailingComma: "es5",
-  printWidth: 100,
-  plugins: ["prettier-plugin-tailwindcss"],
 };
